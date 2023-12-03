8294e59b0bcd89d0e0ffe119da6a494d877c84ad
Parth Patel <parth2002715@gmail.com>
2023-12-03 12:42:21 -0500

    init commit

---
+++ src/app/Project/card.ts
@@ -0,0 +1,5 @@
+export class Card {
+    id: string = '';
+    suit: string = '';
+    value: number = 0;
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from './Project/home/home.component';
 import { RouterModule } from '@angular/router';
-import { RulesComponent } from './Project/rules/rules.component';
-import { PlayComponent } from './Project/play/play.component';
 
 @NgModule({
-  declarations: [
-    HomeComponent,
-    RulesComponent,
-    PlayComponent
-  ],
+  declarations: [],
   imports: [
     RouterModule,
     CommonModule
